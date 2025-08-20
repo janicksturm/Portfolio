cc0c563c5ef7c23134f1b465bd314d47870f7e8b
Janick Sturm <sturmjanick@gmail.com>
2025-08-20 15:36:01 +0200

    add speed up feature

--- src/js/clicker.js
+++ src/js/clicker.js
@@ -1,9 +1,12 @@
 let counter_div = document.getElementById("counter_div");
 let button = document.getElementById("click_button");
 let upgrade_button = document.getElementById("upgrade_button");
+let time_button = document.getElementById("time_button");
 
 let counter_num = 0;
 let auto_clicks = 0;
+let time_var = 1000;
+let reduce_time = 0;
 
 function getRandomColor() {
     let symbols = "0123456789ABCDEF"
@@ -18,6 +21,7 @@ function getRandomColor() {
 let display_counter = document.createElement("p");
 function updateDisplay() {
     upgrade_button.style.display = "inline-block";
+    time_button.style.display = "inline-block";
     upgrade_button.innerText = "Auto-Click +" + auto_clicks;
     display_counter.innerHTML = "Klicks: " + counter_num;
     counter_div.appendChild(display_counter);
@@ -40,10 +44,19 @@ upgrade_button.addEventListener("click", () => {
     }
 });
 
+time_button.addEventListener("click", () => {
+    if (counter_num > 50) {
+        counter_num -= 50;
+        reduce_time++;
+        time_var -= reduce_time;
+        time_button.innerText = "Zeit +" + reduce_time;
+    }
+});
+
 setInterval(() => {
     if (auto_clicks > 0) {
         counter_num += auto_clicks;
         updateDisplay();
     }
-}, 1000);
+}, time_var);
 
