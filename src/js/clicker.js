c343a131ef07a855e9062ea7eb7735674ca11380
Janick Sturm <sturmjanick@gmail.com>
2025-08-20 15:17:56 +0200

    add auto click feature

--- src/js/clicker.js
+++ src/js/clicker.js
@@ -1,7 +1,9 @@
 let counter_div = document.getElementById("counter_div");
 let button = document.getElementById("click_button");
+let upgrade_button = document.getElementById("upgrade_button");
 
 let counter_num = 0;
+let auto_clicks = 0;
 
 function getRandomColor() {
     let symbols = "0123456789ABCDEF"
@@ -13,12 +15,35 @@ function getRandomColor() {
     button.style.backgroundColor = color;
 }
 
-getRandomColor();
 let display_counter = document.createElement("p");
-button.addEventListener("click", () => {
-    counter_num++;
+function updateDisplay() {
+    upgrade_button.style.display = "inline-block";
+    upgrade_button.innerText = "Auto-Click +" + auto_clicks;
     display_counter.innerHTML = "Klicks: " + counter_num;
     counter_div.appendChild(display_counter);
+}
+
+getRandomColor();
+button.addEventListener("click", () => {
+    counter_num++;
+    updateDisplay();
     getRandomColor();
 }
 );
+
+upgrade_button.addEventListener("click", () => {
+    if (counter_num > 9) {
+        counter_num -= 10;
+        auto_clicks += 2;
+        upgrade_button.innerText = "Auto-Click +" + auto_clicks;
+        updateDisplay();
+    }
+});
+
+setInterval(() => {
+    if (auto_clicks > 0) {
+        counter_num += auto_clicks;
+        updateDisplay();
+    }
+}, 1000);
+
